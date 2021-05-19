9b83406563e7156d633277a44db893476beb9980
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2021-05-19 15:53:54 -0500

    added some scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import './App.scss';
+
 function App() {
   return (
     <div className="App">
-      <div className="sidebar"></div>
-      <div className="main-content"></div>
+      <div className="sidebar">
+        <NavBar />
+      </div>
+      <div className="main-content">
+
+      </div>
     </div>
   )
 }
