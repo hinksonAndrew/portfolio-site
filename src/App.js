87d32df0e0af9ade165afe4c0f615ee10d003bab
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2021-05-21 09:38:39 -0500

    fixed homepage added icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss';
 import NavBar from './Components/NavBar';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
         <NavBar />
       </div>
       <div className="main-content">
+        <div className="content">
+          <HomePage />
+        </div>
       </div>
     </div>
     
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom';
 
 function HomePage() {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="HomePage">
+      <header className="hero">
+        <h1 className="hero-text">
+          Hello, I am 
+          <span> Andrew Hinkson.</span>
+        </h1>
+        <p className="h-sub-text">
+          lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolar lorem ipsum dolarlorem ipsum dolarlorem ipsum dolarlorem ipsum dolarlorem ipsum dolarlorem ipsum dolarlorem ipsum dolarlorem ipsum dolarlorem ipsum dolarlorem ipsum dolar
+        </p>
+        <div className="icons">
+          <Link className="icon-holder">
+            <FontAwesomeIcon icon={faFacebook} className="icon fb" />
+          </Link>
+          <Link className="icon-holder">
+            <FontAwesomeIcon icon={faGithub} className="icon gh" />
+          </Link>
+          <Link className="icon-holder">
+            <FontAwesomeIcon icon={faYoutube} className="icon yt" />
+          </Link>
+        </div>
+      </header>
     </div>
   )
 }
