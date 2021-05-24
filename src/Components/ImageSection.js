5a5b09e7f11b329440a9644d20a0503be1c3de60
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2021-05-24 12:03:37 -0500

    finish about section/imagesection

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import about from '../img/about.png';
+import about from '../img/about.jpg';
 
 function ImageSection() {
   return (
@@ -9,14 +9,30 @@ function ImageSection() {
       </div>
       <div className="about-info">
         <h4>I am<span> Andrew Hinkson</span></h4>
-        <p>
+        <p className="about-text">
           Lorem ipsumLorem ipsumLorem ipsumLorem 
           ipsumLorem ipsumLorem ipsumLorem ipsumLorem 
           ipsumLorem ipsumLorem ipsumLorem ipsumLorem 
         </p>
         <div className="about-details">
-
+          <div className="left-section">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Nationality</p>
+            <p>Languages</p>
+            <p>Address</p>
+            <p>Countries</p>
+          </div>
+          <div className="right-section">
+            <p>: Andrew Hinkson</p>
+            <p>: 33</p>
+            <p>: American</p>
+            <p>: English</p>
+            <p>: 123 Main St, Nowhere, Iowa</p>
+            <p>: United States</p>
+          </div>
         </div>
+        <button className="btn">Download CV</button>
       </div>
     </div>
   )
