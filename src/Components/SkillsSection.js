48f9f1378a8a6faeb4492f748cc218e57dca57cc
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2021-08-09 12:19:21 -0500

    update

--- src/Components/SkillsSection.js
+++ src/Components/SkillsSection.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-function SkillsSection() {
+function SkillsSection({skill, progress, width}) {
   return (
-    <div>
-      
+    <div className="SkillsSection">
+      <div className="skills-container">
+        <h5 className="skill-title">{skill}</h5>
+        <div className="skill-bar">
+          <p className="skill-text">{progress}</p>
+          <div className="skill-progress">
+            <div className="progress">
+              <div className="inner-progress" style={{width:width}}></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Title from '../Components/Title';
 import ImageSection from '../Components/ImageSection';
+import SkillsSection from '../Components/SkillsSection';
 
 function AboutPage() {
   return (
     <div className="AboutPage">
       <Title title={'About Me'} span={'About Me'} />
       <ImageSection />
+      <Title title={'My Skills'} span={'My Skills'} />
+      <div className="skills-container">
+        <SkillsSection skill={'JavaScript'} progress={'60%'} width={'60%'} />
+        <SkillsSection skill={'TypeScript'} progress={'30%'} width={'30%'} />
+        <SkillsSection skill={'React JS'} progress={'70%'} width={'70%'} />
+        <SkillsSection skill={'Node JS'} progress={'25%'} width={'25%'} />
+        <SkillsSection skill={'HTML'} progress={'50%'} width={'50%'} />
+        <SkillsSection skill={'CSS'} progress={'50%'} width={'50%'} />
+      </div>
     </div>
   )
 }
